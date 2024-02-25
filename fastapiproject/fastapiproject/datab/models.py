b374c1ada0701a7188a021a8659e352ef1585224
Симонов Алексей <alxxalxx@mail.ru>
2024-02-25 20:36:18 +0300

    create migrations and user table

--- fastapiproject/fastapiproject/datab/models.py
+++ fastapiproject/fastapiproject/datab/models.py
@@ -4,9 +4,11 @@ from config import settings
 
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(url=settings.DATABASE_URL,
-                       echo=True,
-                       pool_size=5,
-                       max_overflow=10)
+engine = create_engine(
+    url=settings.DATABASE_URL,
+    echo=True,
+    pool_size=5,
+    max_overflow=10
+)
 
 session_factory = sessionmaker(bind=engine)
